deb5c5c54aea0a8a19f7517a72b7efee37a64e08
lukas <lil.luka1997@gmail.com>
2022-01-26 19:36:34 +0200

    kek)

--- index.js
+++ index.js
@@ -16,56 +16,87 @@ const json = fs.readFileSync('weekends.json', 'utf-8')
 
 const object = JSON.parse(json)
 
+const arr = []
 
+object.forEach(el => {
+    let present = false
+    arr.forEach(val => {
+        if (val.name === el.user.name) {
+            present = true
 
-object.forEach(item => {
-    delete item.status;
-    delete item.usedDays;
-    let name = item.user.name
-    let user = item.user._id
-    delete item.user
-    item.userId = user
-    item.name = name
-
-    let week = {}
-    week.startDate = item.startDate
-    week.endDate = item.endDate
-    item.weekends = [week]
-
-    delete item.startDate
-    delete item.endDate
-    delete item._id
-
-});
-
-// console.log(object)
-
-// let newObj = []
-let arr = []
-let setNames = new Set()
-let setObjects = new Set(object)
-let firstSize = setNames.size
-
-
-object.forEach(value => {
-    setNames.add(value.name)
-    if (firstSize === setNames.size) arr.push(value)
-
-    firstSize = setNames.size
-})
-
-setObjects.forEach(val => {
-    arr.forEach(value => {
-        if (value.name === val.name){
-            let newDate = value.weekends
-            console.log(newDate)
-            val.weekends.push()
+            val.weekendDates.push({
+                startDate: el.startDate,
+                endDate: el.endDate
+            })
         }
     })
+
+    if (!present) {
+        const weekendDates = [{
+            startdate: el.startDate,
+            endDate: el.endDate
+        }]
+
+        const obj = {
+            userId: el._id,
+            name: el.user.name,
+            weekendDates
+        }
+        arr.push(obj)
+    }
 })
 
-
-console.log(setObjects)
+console.log(arr)
+//
+//
+// object.forEach(item => {
+//     delete item.status;
+//     delete item.usedDays;
+//     let name = item.user.name
+//     let user = item.user._id
+//     delete item.user
+//     item.userId = user
+//     item.name = name
+//
+//     let week = {}
+//     week.startDate = item.startDate
+//     week.endDate = item.endDate
+//     item.weekends = [week]
+//
+//     delete item.startDate
+//     delete item.endDate
+//     delete item._id
+//
+// });
+//
+// // console.log(object)
+//
+// // let newObj = []
+// let arr = []
+// let setNames = new Set()
+// let setObjects = new Set(object)
+// let firstSize = setNames.size
+//
+//
+// object.forEach(value => {
+//     setNames.add(value.name)
+//     if (firstSize === setNames.size) arr.push(value)
+//
+//     firstSize = setNames.size
+// })
+//
+// setObjects.forEach(val => {
+//     arr.forEach(value => {
+//         if (value.name === val.name) {
+//             let newDate = value.weekends.values()
+//             console.log(newDate)
+//             val.weekends.push(newDate)
+//         }
+//     })
+// })
+//
+//
+// console.log(setObjects)
 
 // object.forEach( => {
 //
